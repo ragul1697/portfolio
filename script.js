9a7de7606707371b6ce1fe5df790dcd86f0efbb8
ragul.u <ragul.u@kgisl.com>
2024-05-20 16:01:13 +0530

    profile details updated

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const workEls = document.querySelectorAll(".work-box");
 const workImgs = document.querySelectorAll(".work-img");
 const mainEl = document.querySelector("main");
 const yearEl = document.querySelector(".footer-text span");
+const emailId = 'ragulshankar1697@gmail.com';
+
+yearEl.textContent = new Date().getFullYear();
+
+function onSendEmailClick() {
+  window.open(`mailto:${emailId}`);
+}
 
 const toggleNav = () => {
   nav.classList.toggle("hidden");
@@ -91,24 +98,4 @@ document.body.addEventListener("keydown", (e) => {
   }
 });
 
-// Rotating logos animation
 
-const logosWrappers = document.querySelectorAll(".logo-group");
-
-const sleep = (number) => new Promise((res) => setTimeout(res, number));
-
-logosWrappers.forEach(async (logoWrapper, i) => {
-  const logos = Array.from(logoWrapper.children);
-  await sleep(1400 * i);
-  setInterval(() => {
-    let temp = logos[0];
-    logos[0] = logos[1];
-    logos[1] = logos[2];
-    logos[2] = temp;
-    logos[0].classList.add("hide", "to-top");
-    logos[1].classList.remove("hide", "to-top", "to-bottom");
-    logos[2].classList.add("hide", "to-bottom");
-  }, 5600);
-});
-
-yearEl.textContent = new Date().getFullYear();
